a064aa8312968c70169896758489a04ba54ce255
Mary McMurray and Scott Bergler <scott@>
2018-10-22 11:23:54 -0700

    refactored js using foreach

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,30 @@ $(document).ready(function() {
   $("form#list").submit(function(event) {
     event.preventDefault();
 
-    var firstItem = $("input#itemOne").val();
-    var secondItem = $("input#itemTwo").val();
-    var thirdItem = $("input#itemThree").val();
-    var fourthItem = $("input#itemFour").val();
-    var fifthItem = $("input#itemFive").val();
+    // var firstItem = $("input#itemOne").val();
+    // var secondItem = $("input#itemTwo").val();
+    // var thirdItem = $("input#itemThree").val();
+    // var fourthItem = $("input#itemFour").val();
+    // var fifthItem = $("input#itemFive").val();
 
-    var groceryItems = [firstItem, secondItem, thirdItem, fourthItem, fifthItem];
+    var items = ["itemOne", "itemTwo", "itemThree", "itemFour", "itemFive"];
 
-    var firstThreeItems = [];
-    firstThreeItems.push(groceryItems[0]);
-    firstThreeItems.push(groceryItems[1]);
-    firstThreeItems.push(groceryItems[2]);
+    // var groceryItems = [firstItem, secondItem, thirdItem, fourthItem, fifthItem];
 
-    $("#first").text(firstThreeItems[0]);
-    $("#second").text(firstThreeItems[1]);
-    $("#third").text(firstThreeItems[2]);
+    // var firstThreeItems = [];
+
+    // firstThreeItems.push(groceryItems[0]);
+    // firstThreeItems.push(groceryItems[1]);
+    // firstThreeItems.push(groceryItems[2]);
+    //
+    // $("#first").text(firstThreeItems[0]);
+    // $("#second").text(firstThreeItems[1]);
+    // $("#third").text(firstThreeItems[2]);
+
+    items.forEach(function(item) {
+      var userInput = $("input#" + item).val();
+      $("#groceryList").append("<li>" + userInput + "</li>");
+    });
+
   });
 });
